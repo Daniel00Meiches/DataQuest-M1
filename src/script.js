8754cffd399c7f8a28b32b1226479dda1bc5a3b9
Kaian Moura <kaian.moura@sou.inteli.edu.br>
2025-02-11 14:03:57 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function selecionarFase(fase) {
+    alert("Você selecionou a Fase " + fase);
+}
+
+function abrirPopup() {
+    document.getElementById("popup-como-jogar").style.display = "flex";
+}
+
+function fecharPopup() {
+    document.getElementById("popup-como-jogar").style.display = "none";
+}
